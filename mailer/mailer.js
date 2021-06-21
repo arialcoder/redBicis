da94076e8422df842a9324ddaef6a62cbf9a7138
miriam <xinerama@gmail.com>
2021-06-21 08:12:30 -0300

    sendgridv

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,13 +1,44 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+let mailConfig;
+// ENVIRONMENT HEROKU
+if (process.env.NODE_ENV === "production") {
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET,
+        },
+    };
+    mailConfig = sgTransport(options);
+} else {
+    if (process.env.NODE_ENV === "staging") {
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET,
+            },
+        };
+        mailConfig = sgTransport(options);
+    } else {
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ETHEREAL_EMAIL_USER_DEV,
+                pass: process.env.ETHEREAL_EMAIL_PASS_DEV
+            },
+        };
+    }
+}
  
- //Configurar cuenta de https://ethereal.email  
- const mailConfig = {
-     host: 'smtp.ethereal.email',
-     port: 587,
-     auth: {
-         user: '',
-         pass: ''
-     }
- };
+
+//Configurar cuenta de https://ethereal.email  
+//  const mailConfig = {
+//      host: 'smtp.ethereal.email',
+//      port: 587,
+//      auth: {
+//          user: '',
+//          pass: ''
+//      }
+//  };
 
 module.exports = nodemailer.createTransport(mailConfig);
