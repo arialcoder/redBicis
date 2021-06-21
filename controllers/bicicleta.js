31b1e1bb623104c4d996ee392a1f4d25ee1c7bd2
miriam <xinerama@gmail.com>
2021-06-21 07:42:25 -0300

    add dotenv

--- controllers/bicicleta.js
+++ controllers/bicicleta.js
@@ -10,49 +10,68 @@ var Bicicleta = require('../models/bicicleta');
     
   }
 
-exports.bicicleta_create_get = function(req,res){
+  exports.bicicleta_create_get = function(req,res){
     res.render('bicicletas/create');
-}
-
-exports.bicicleta_create_post = function(req,res){
-   var bici = new Bicicleta({ code: req.body.code, color: req.body.color, modelo: req.body.modelo });
-   bici.ubicacion = [req.body.lat, req.body.lng];
-   Bicicleta.add(bici);
-
-   res.redirect('/bicicletas');
-}
-
-exports.bicicleta_update_get = function(req,res){
-    Bicicleta.findById(req.params.id, function(err, bici){ console.log(bici);
-        res.render('bicicletas/update', {bici});
-    });
-}
-
-exports.bicicleta_update_post = function(req,res){
-    var update_values = { 
-        color : req.body.color,
-        modelo: req.body.modelo,
-        ubicacion : [req.body.lat, req.body.lng]
-    };
-    Bicicleta.findByIdAndUpdate(req.params.id, update_values,
-      (err, bicicleta) => {
-      //if there is a error we render the View with a error
-      if (err) {
-        console.log(err);
-        res.render('bicicletas/update', {
-          errors: err.errors,
-          bicicleta
-        })
-      } else {
-        //If everthing is Ok we render to bicicletas
-        res.redirect('/bicicletas');
-        return;
-      }
-    })
   }
 
-exports.bicicleta_delete_post = function(req,res){
-    Bicicleta.removeById(req.body.id, (err, raw) =>{
-        res.redirect('/bicicletas');
-    });   
+
+// exports.bicicleta_create_post = function(req, res){
+//   var bici = new Bicicleta(req.body.id, req.body.color, req.body.modelo)
+//   bici.ubicacion = [req.body.lat, req.body.lng]
+//   Bicicleta.add(bici)
+//   res.redirect('/bicicletas')
+// }
+exports.bicicleta_create_post = function (req, res) {
+  let bici = new Bicicleta({
+   code: req.body.code,
+   color: req.body.color,
+   modelo: req.body.modelo,
+   ubicacion: [req.body.lat || 0, req.body.lng || 0]
+ });
+ console.log("va a guardar las bicicletas" + bici);
+ Bicicleta.add(bici)
+ res.redirect('/bicicletas');
 }
+
+exports.bicicleta_update_get = function (req, res) {
+  console.log("req.params", req.params)
+  Bicicleta.findById(req.params.id).exec((err, bici) => {
+    res.render('bicicletas/update', {
+      bici
+    });
+  })
+}
+// exports.bicicleta_update_get = function(req, res){
+//    Bicicleta.findById(req.params.id, function(err,bicicleta){
+//      console.log('code:'+ req.params.id);
+//     res.render('bicicletas/update');
+//   })
+  
+// }
+
+exports.bicicleta_update_post = function(req, res){
+  var bici = Bicicleta.findById(req.params.id)
+  bici.id = req.body.id
+  bici.color = req.body.color
+  bici.modelo = req.body.modelo
+  bici.ubicacion = [req.body.lat, req.body.lng]
+
+  Bicicleta.findByIdAndUpdate(req.params.id,bici, function(err){
+    if (err)
+    next(err);
+      else
+      res.redirect('/bicicletas');
+  });
+  
+}
+
+exports.bicicleta_delete_post = function (req, res) {
+  console.log("delete",req.body.id);
+  Bicicleta.findByIdAndDelete (req.body.id, function(err){
+    if (err)
+    next(err);
+      else
+    res.redirect('/bicicletas');
+
+  });
+} 
